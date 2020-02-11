22b0e23d603fef07bcfd8eabf447790bfd837d3b
zawa-ch <lunatic.zawa.ch@gmail.com>
2020-02-11 23:21:11 +0900

    C++ rvalue reference test 3

--- C/202002112225.cpp
+++ C/202002112225.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
+#include <utility>
 
 class AbstractClass
 {
 public:
-    virtual void foo() const = 0;
+    virtual void foo() const = 0; // 純粋仮想関数の宣言(このクラスはabstractとなる)
 };
 
 class SubClass : virtual public AbstractClass
 {
 public:
-    void foo() const { std::cout << "do something" << std::endl; }
+    void foo() const { std::cout << "do something" << std::endl; } // AbstractClass::fooの実装
 };
 
 class AbstractHolder
@@ -18,10 +19,16 @@ public:
     virtual AbstractClass&& get() const = 0;
 };
 
-class SubHolder
+class SubHolder1
 {
 public:
-    AbstractClass&& get() const { return SubClass(); } // オブジェクトの生存期間はこの関数内のみ
+    AbstractClass&& get() const { return SubClass(); } // 転送参照ではないため、オブジェクトの生存期間はこの関数内のみ
+};
+
+class SubHolder2
+{
+public:
+    AbstractClass&& get() const { return std::forward<AbstractClass>(SubClass()); } // 転送参照、オブジェクトの生存期間が延長される
 };
 
 int main()
@@ -29,9 +36,13 @@ int main()
     AbstractClass&& rref1 = SubClass(); // OK、オブジェクトはmain関数内で生存する
     rref1.foo();
 
-    SubHolder holder = SubHolder();
-    AbstractClass&& rref2 = holder.get(); // エラー、破棄されたオブジェクトへの参照
+    SubHolder1 holder1 = SubHolder1();
+    AbstractClass&& rref2 = holder1.get(); // エラー、破棄されたオブジェクトへの参照
     rref2.foo();
+
+    SubHolder2 holder2 = SubHolder2();
+    AbstractClass&& rref3 = holder2.get(); // OK、std::forwardを使用して転送されるため、生存期間が延長される
+    rref3.foo();
 
     return 0;
 }
