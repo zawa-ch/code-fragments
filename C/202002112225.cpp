a658a2723bec9cc82e23d6faba5f20b6c20ec1ec
zawa-ch <lunatic.zawa.ch@gmail.com>
2020-02-11 22:57:49 +0900

    C++ rvalue reference test 2

--- C/202002112225.cpp
+++ C/202002112225.cpp
@@ -12,9 +12,26 @@ public:
     void foo() const { std::cout << "do something" << std::endl; }
 };
 
+class AbstractHolder
+{
+public:
+    virtual AbstractClass&& get() const = 0;
+};
+
+class SubHolder
+{
+public:
+    AbstractClass&& get() const { return SubClass(); } // オブジェクトの生存期間はこの関数内のみ
+};
+
 int main()
 {
-    AbstractClass&& rref = SubClass();
-    rref.foo();
+    AbstractClass&& rref1 = SubClass(); // OK、オブジェクトはmain関数内で生存する
+    rref1.foo();
+
+    SubHolder holder = SubHolder();
+    AbstractClass&& rref2 = holder.get(); // エラー、破棄されたオブジェクトへの参照
+    rref2.foo();
+
     return 0;
 }
